830e87c5d8cb0beb6fcbfa77fae83566654a0022
Miguel Henriquez <henriquez.miguel@outlook.com>
2018-11-24 00:00:24 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,14 @@
 # Write your #display_rainbow method here
+Colors = 
+
+
+
+
 display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+ 
+ 
   
-def print_business_card(person)
+def (person)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
