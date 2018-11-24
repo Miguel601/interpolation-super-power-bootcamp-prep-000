c650bb3f7d23bbc9f910b98ade2e07e759e0e079
Miguel Henriquez <henriquez.miguel@outlook.com>
2018-11-24 00:02:34 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,4 +8,4 @@ Display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet
   
 def (person)
   puts "R: #{person[0]}, O: #{person[1]}, Y: #{person[2]}"
-  puts "G: #{person[3]}" 
+  puts "G: #{person[3]}" "G: #{person[3]}" 
